cd14bb68c04ae8ba8526c5749c536f84929995f3
LiaoWorking <liaoworking@gmail.com>
2019-12-14 12:34:11 +0800

    First Commit

---
+++ Travis_CI_Demo/Travis_CI_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travis_CI_Demo
+//
+//  Created by Run Liao on 2019/12/14.
+//  Copyright © 2019 Run Liao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
